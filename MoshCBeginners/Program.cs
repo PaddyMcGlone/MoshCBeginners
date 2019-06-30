56b0ac10dc598d19e377e30e991e1796edf2e83b
Paddy McGlone <paddymcglone@gmail.com>
2019-06-30 12:55:39 +0100

    Adding the initial commit
    - Adding Types
    - Adding non primitative types

---
+++ MoshCBeginners/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MoshCBeginners
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Implicit type conversion
+            //byte myByte = 255;
+            //int myInt = myByte;
+
+            //Console.WriteLine("Implicit type conversion =  "+ myInt);
+
+            // Explicit type conversion
+            //byte myByte = 0;
+            //int myInt = 200;
+
+            //myByte = (byte)myInt;
+            //Console.WriteLine("Explicit type conversion = " + myByte);
+
+            // Convert string to an int
+            string myString = "100";
+            int myInt = Convert.ToInt32(myString);
+
+            Console.WriteLine($"Convert class = {myInt}");
+
+            // Convert string to a boolean
+            string myBooleanString = "True";
+            bool myBool = Convert.ToBoolean(myBooleanString);
+            
+            Console.WriteLine(myBool ? "Correct" : "False");
+        }
+    }
+}
---
+++ NonPrimitiveType/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NonPrimitiveType
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Creating string
+            var firstName = "Patrick";
+            var lastName = "McGlone";
+
+            Console.WriteLine(String.Format("{0} {1}", firstName, lastName));
+
+            // Using String.Join
+            var myNumbers = new[] { 1, 2, 3, 4, 5, 6, 100, 200, 300 };
+            Console.WriteLine(string.Join(" - ", myNumbers));
+        }
+    }
+}
