756ff316fc3ad19c3949af7e573c1f574012804e
Paddy McGlone <paddymcglone@gmail.com>
2019-07-08 19:35:33 +0100

    Update Program.cs
    Adding new challenge

--- ArraysAndlistsII/Program.cs
+++ ArraysAndlistsII/Program.cs
@@ -40,25 +40,47 @@ namespace ArraysAndlistsII
             // 2- Write a program and ask the user to enter their name.Use an array to reverse the name and then store the result in a new string.
             // Display the reversed name on the console.
 
-            Console.WriteLine("Please enter your name");
-            var input = Console.ReadLine();
-            var length = input.Length - 1;
-            var result1 = "";
+            //Console.WriteLine("Please enter your name");
+            //var input = Console.ReadLine();
+            //var length = input.Length - 1;
+            //var result1 = "";
 
-            for (int i = length; i >= 0; i--)
-                result1 += input[i];
+            //for (int i = length; i >= 0; i--)
+            //    result1 += input[i];
 
-            Console.WriteLine($"First attempt at reversing {result1}");
+            //Console.WriteLine($"First attempt at reversing {result1}");
 
 
-            Console.WriteLine("Please enter your name again!");
-            var nameArray = Console.ReadLine().ToCharArray();
-            Array.Reverse(nameArray);
-            
-            Console.WriteLine($"Second attempt at reversing {string.Join("",nameArray)}");
+            //Console.WriteLine("Please enter your name again!");
+            //var nameArray = Console.ReadLine().ToCharArray();
+            //Array.Reverse(nameArray);
+
+            //Console.WriteLine($"Second attempt at reversing {string.Join("",nameArray)}");
+
+            // 3- Write a program and ask the user to enter 5 numbers.If a number has been previously entered,
+            // display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
+
+            var count = 1;
+            var numbers = new List<int>();
+            do
+            {
+                Console.WriteLine("Please enter a number");
+                var number = Convert.ToInt32(Console.ReadLine());
+
+                if (numbers.Contains(number))
+                {
+                    Console.WriteLine("Number previously entered, please re-try");
+                    continue;
+                }
+
+                numbers.Add(number);
+                count++;
+            }
+            while (count <= 5);
+
+            numbers.Sort();
+            Console.WriteLine($"Sorted numbers {string.Join("-",numbers)}");            
         }
-
-
 
     }
 }
