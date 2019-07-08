dae340d16d704becd7729dc9a4a01a42ef2eb402
Paddy McGlone <paddymcglone@gmail.com>
2019-07-08 19:21:05 +0100

    Adding the new scenario
    - Adding the new challenges

--- ArraysAndlistsII/Program.cs
+++ ArraysAndlistsII/Program.cs
@@ -18,24 +18,47 @@ namespace ArraysAndlistsII
             //    Depending on the number of names provided, display a message based on the above pattern.
 
 
-            var names = new List<string>();
-            while (true)
-            {
-                Console.WriteLine("Please enter a name:");
-                var name = Console.ReadLine();
+            //var names = new List<string>();
+            //while (true)
+            //{
+            //    Console.WriteLine("Please enter a name:");
+            //    var name = Console.ReadLine();
 
-                if (string.IsNullOrWhiteSpace(name)) break;
+            //    if (string.IsNullOrWhiteSpace(name)) break;
 
-                names.Add(name);
-            }
+            //    names.Add(name);
+            //}
+
+            //if(names.Count == 1)
+            //    Console.WriteLine($"{names[0]} likes your post");
+            //else if(names.Count == 2)
+            //    Console.WriteLine($"{names[0]}, {names[1]} like your post");
+            //else
+            //    Console.WriteLine($"{names[0]}, {names[1]} and {names.Count - 2} others like your post");
+
+
+            // 2- Write a program and ask the user to enter their name.Use an array to reverse the name and then store the result in a new string.
+            // Display the reversed name on the console.
+
+            Console.WriteLine("Please enter your name");
+            var input = Console.ReadLine();
+            var length = input.Length - 1;
+            var result1 = "";
+
+            for (int i = length; i >= 0; i--)
+                result1 += input[i];
+
+            Console.WriteLine($"First attempt at reversing {result1}");
+
+
+            Console.WriteLine("Please enter your name again!");
+            var nameArray = Console.ReadLine().ToCharArray();
+            Array.Reverse(nameArray);
             
-            if(names.Count == 1)
-                Console.WriteLine($"{names[0]} likes your post");
-            else if(names.Count == 2)
-                Console.WriteLine($"{names[0]}, {names[1]} like your post");
-            else
-                Console.WriteLine($"{names[0]}, {names[1]} and {names.Count - 2} others like your post");
-
+            Console.WriteLine($"Second attempt at reversing {string.Join("",nameArray)}");
         }
-}
+
+
+
+    }
 }
