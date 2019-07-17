37aec69525f98dba4ebe5e7dd2d55721de3993b1
Paddy McGlone <paddymcglone@gmail.com>
2019-07-17 20:05:33 +0100

    Adding the file project
    - Adding project
    - Adding new exercises

---
+++ Files/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //1 - Write a program that reads a text file and displays the number of words.
+            //var file = File.ReadAllText("/Users/patrickmcglone/Documents/sampletext");
+            //Console.WriteLine($"There are {file.Split(' ').Length} words in this file");
+
+            //2 - Write a program that reads a text file and displays the longest word in the file.
+            var myFile = File.ReadAllText("/Users/patrickmcglone/Documents/sampletext");
+            var longestWord = "";
+
+            foreach (var word in myFile.Split(' '))
+                longestWord = longestWord.Length < word.Length ? word : longestWord;
+
+            Console.WriteLine($"The longest word within your file is : {longestWord}");
+        }
+    }
+}
