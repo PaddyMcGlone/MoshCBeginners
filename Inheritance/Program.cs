83e5c7aaa899e488d421e8309d9e75a561c28ace
Paddy McGlone <paddymcglone@gmail.com>
2019-07-27 16:34:30 +0100

    Adding the new project
    Adding some new notes for inheritance studying.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Shape
+    {
+        public string Area { get; set; }
+    }
+
+    public class Circle : Shape
+    {
+        public string Radius { get; set; }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Downcasting example
+            var myShape = new Shape();
+            myShape = new Circle();
+
+
+            // In order to prevent the invalid cast exception we can use the 'as' keyword
+            Shape newShape = (Circle) new Shape(); // Will return an exception if we cannot cast
+
+            newShape  = newShape as Circle; // 'as' keyword will not return exception, instead will allow compilation to continue.
+            if(newShape != null)
+            {
+                //... Do some work.
+            }
+
+
+            // We can use the 'is' keyword to work out if an object is a certain type
+            if(newShape is Shape)
+            {
+                //... Do some work
+            }
+        }
+    }
+}
