a46e34dd628581760b9ad576098ffc81a4c3e085
Paddy McGlone <paddymcglone@gmail.com>
2019-07-28 16:03:16 +0100

    Adding Polymorphisim solution
    - Adding Polymorphisim detais
    - Adding Abstract class details

---
+++ Polymorphisim/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Polymorphisim
+{
+    public class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("The base class draw.");
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle.");
+        }
+    }
+
+    // Below is an example of polymorphisim, we are overriding the method draw for our specific purposes.
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var shape = new Shape();
+            shape.Draw();
+
+            var circle = new Circle();
+            circle.Draw();
+        }
+    }
+
+    // The Benifit of this approach is we could add another class and it wouldnt have any effect on the shape class nor the other sister child classes.
+    // This is an example of a perfect lose couple relationship, we can make a change and it doesnt effect other areas of the application.
+
+
+    // Abstract classes
+    // This is where within the base class we do not define the method implementation, as it is too hard to define a 'draw' method for all the different types
+    // of shapes.
+
+    public abstract class AbstractShape
+    {
+        // No implementation details
+        public abstract void Draw();
+        
+    }
+
+    public class NewCircle : AbstractShape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Adding new implementation details.");
+        }
+    }
+}
