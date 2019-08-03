b173133ebc9d9e202bc7afb73c52e9938a0a1bbc
Paddy McGlone <paddymcglone@gmail.com>
2019-08-03 14:51:45 +0100

    Update PhotoProcessor.cs
    - Adding the new delegate.

--- Delegates/PhotoProcessor.cs
+++ Delegates/PhotoProcessor.cs
@@ -1,10 +1,18 @@
 ﻿namespace Delegates
 {
+    // Creating a delegate, which will hold the pointer address
+    // of all the methods that we wish to call.
+    public delegate void PhotoFilterHander(Photo photo);
+
     public class PhotoProcessor
     {
-        public void Process(Photo photo)
+        // Below we pass the delegate into our method as a param.
+        public void Process(Photo photo, PhotoFilterHander photoFilterHander)
         {
             var filter = new PhotoFilter();
+            photoFilterHander(photo); // Call the delegate with the photo.
+
+            // Above the delegate is now incharge of handling all the photo filters to be applied.
         }
     }
 }
