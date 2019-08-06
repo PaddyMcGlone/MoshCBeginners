2b2ade7a821546f82ec269ccbfabe5353fe168dc
Paddy McGlone <paddymcglone@gmail.com>
2019-08-06 19:00:51 +0100

    Adding basic data
    - Adding the basic info.

---
+++ EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EventsAndDelegates
+{
+    internal class VideoEncoder
+    {
+        public VideoEncoder()
+        {
+        }
+
+        internal void Encode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
