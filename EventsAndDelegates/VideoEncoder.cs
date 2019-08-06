d7093bae4250db077aa37a331445621c1c30bb29
Paddy McGlone <paddymcglone@gmail.com>
2019-08-06 19:24:10 +0100

    Update VideoEncoder.cs
    - Adding the delegate and the event

--- EventsAndDelegates/VideoEncoder.cs
+++ EventsAndDelegates/VideoEncoder.cs
@@ -5,9 +5,21 @@ namespace EventsAndDelegates
 {
     internal class VideoEncoder
     {
+        // 1. Define a delegate
+        // 2. Define an event
+        // 3. Raise the event
+
         public VideoEncoder()
         {
         }
+
+        // 1. Delegate
+        // object Source = The source which is raising the event
+        // EventArgs = Any additional data which we want to send with the event 
+        public delegate void VideoEnocderEventHandler(object source, EventArgs args);
+
+        // 2. Event
+        public event VideoEnocderEventHandler VideoEncoded;
 
         internal void Encode(Video video)
         {
