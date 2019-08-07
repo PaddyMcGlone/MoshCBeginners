fc47d6ae7fada53bb6fc73c4c487086a91cb0f48
Paddy McGlone <paddymcglone@gmail.com>
2019-08-07 19:09:59 +0100

    Adding new messaging service
    - Adding WhatsApp messaging service.

--- EventsAndDelegates/Program.cs
+++ EventsAndDelegates/Program.cs
@@ -7,12 +7,17 @@
             var video1 = new Video { Title = "Van highlights" };
             var encoder = new VideoEncoder(); // Publisher
             var mailService = new MailService(); // Subscriber
+            var whatsApp = new WhatsAppService(); // Another Subscriber
 
             // VideoEncoded behind the scenes is a list of pointers (methods)
             // Below we are just adding a new method to call to this list.
             encoder.VideoEncoded += mailService.VideoEnocderEventHandler; // subcribe to the event
 
+            // Adding another subscriber
+            encoder.VideoEncoded += whatsApp.VideoEnocderEventHandler; // Subscribe to the event
+
             encoder.Encode(video1);
         }
     }
 }
+    
---
+++ EventsAndDelegates/WhatsAppService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventsAndDelegates
+{
+    public class WhatsAppService
+    {
+        public void VideoEnocderEventHandler(object source, EventArgs args)
+        {
+            Console.WriteLine("WhatsAppService: Sending a WhatsApp message.");
+        }
+    }
+}
