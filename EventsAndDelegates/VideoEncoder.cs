e0c9d0d96cc418631a5a285bda5ddc576afa3e09
Paddy McGlone <paddymcglone@gmail.com>
2019-08-07 19:42:16 +0100

    Update VideoEncoder.cs
    - Remcving the custom delegate.

--- EventsAndDelegates/VideoEncoder.cs
+++ EventsAndDelegates/VideoEncoder.cs
@@ -9,15 +9,17 @@ namespace EventsAndDelegates
         // 1. Define a delegate
         // 2. Define an event
         // 3. Raise the event
-        
+
 
         // 1. Delegate
         // object Source = The source which is raising the event
         // EventArgs = Any additional data which we want to send with the event (Or additional custom class)
-        public delegate void VideoEnocderEventHandler(object source, VideoArgs args);
+        //public delegate void VideoEnocderEventHandler(object source, VideoArgs args);
+
 
         // 2. Event
-        public event VideoEnocderEventHandler VideoEncoded;
+        //public event VideoEnocderEventHandler VideoEncoded;
+        public EventHandler<VideoArgs> VideoEncoded; // Using handler, removing the need for a custom delegate.
 
         // 3. Raise the event via a method
         // Convention states they should be marked as protected, virtual and named with 'on'
