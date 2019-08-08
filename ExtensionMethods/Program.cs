9e6749bd64b92f3c0cd66025a0f63b60b47d5147
Paddy McGlone <paddymcglone@gmail.com>
2019-08-08 17:40:57 +0100

    Adding the new module

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
