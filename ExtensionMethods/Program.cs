eeec8b23851b3699c8784926e1b65d54d7f0fcc7
Paddy McGlone <paddymcglone@gmail.com>
2019-08-08 17:46:40 +0100

    Update Program.cs
    - Adding the new extension class.

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -6,7 +6,19 @@ namespace ExtensionMethods
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var myLongString = "My long string which i need shorten to four words";
+            var result = myLongString.Shorten(4); // Calling an extension method on string.
+        }        
+    }
+
+    // Extensions have to be a static class
+    // Naming convention is classname followed by extensions.
+    public static class StringExtensions
+    {
+        public static string Shorten(this string str, int numberOfWords)
+        {
+            return "";
         }
     }
 }
+    
