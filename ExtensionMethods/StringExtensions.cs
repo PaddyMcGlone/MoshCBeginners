6b2f913478497e536b0faa14dd5066920bbafea5
Paddy McGlone <paddymcglone@gmail.com>
2019-08-08 17:57:15 +0100

    Adding the extension method

--- ExtensionMethods/StringExtensions.cs
+++ ExtensionMethods/StringExtensions.cs
@@ -3,17 +3,6 @@ using System.Linq;
 
 namespace ExtensionMethods
 {
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            var myLongString = "My long string which i need shorten to four words";
-            var result = myLongString.Shorten(4); // Calling an extension method on string.
-
-            Console.WriteLine($"The first four words are: {result}");
-        }        
-    }
-
     // Extensions have to be a static class
     // Naming convention is classname followed by extensions.
     public static class StringExtensions
@@ -27,4 +16,3 @@ namespace ExtensionMethods
         }
     }
 }
-    
