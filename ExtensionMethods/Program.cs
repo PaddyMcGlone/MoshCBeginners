caafcbe6446d857270477536af2462fa1e951451
Paddy McGlone <paddymcglone@gmail.com>
2019-08-08 18:28:03 +0100

    Update Program.cs

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace ExtensionMethods
 {
@@ -12,19 +11,6 @@ namespace ExtensionMethods
 
             Console.WriteLine($"The first four words are: {result}");
         }        
-    }
-
-    // Extensions have to be a static class
-    // Naming convention is classname followed by extensions.
-    public static class StringExtensions
-    {
-        public static string Shorten(this string str, int numberOfWords)
-        {
-            if (numberOfWords == 0)
-                return "";
-
-            return String.Join(" ", str.Split(' ').Take(numberOfWords));
-        }
     }
 }
     
