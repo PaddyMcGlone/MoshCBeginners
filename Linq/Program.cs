3651cb9031b8acad8f75b44570b5bcffa7e965f5
Paddy Mc Glone <patrick.mcglone@civica.co.uk>
2019-08-09 10:19:48 +0100

    Update Program.cs
    -Adding Query Operators.

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -11,6 +11,14 @@ namespace Linq
         static void Main(string[] args)
         {
             var books = new BookRepository().GetBooks();
+
+            // Linq Query Operators
+            var cheaperBooks = from b in books
+                               where b.Price < 8
+                               orderby b.Title
+                               select b.Title;
+
+            // Linq Extension Methods
             var cheapBooks = books.Where(b => b.Price < 10).OrderBy(b => b.Title).Select(b => b.Title);
 
             foreach (var book in cheapBooks)
