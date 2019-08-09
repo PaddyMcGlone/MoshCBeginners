feb07459f52c9c03bd6d030668b4cd4989bf9332
Paddy Mc Glone <patrick.mcglone@civica.co.uk>
2019-08-09 09:11:00 +0100

    Adding seeded info

---
+++ Linq/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Linq
+{
+    public class Book
+    {
+        public string Title { get; set; } 
+        public float Price { get; set; } 
+    }
+
+    public class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book {Title = "Lowdown transporters",          Price = 10.99},
+                new Book {Title = "Transporter HQ",                Price = 9.99},
+                new Book {Title = "VW Transporter Haynes mannual", Price = 6.00}
+            };
+        }
+    }
+}
