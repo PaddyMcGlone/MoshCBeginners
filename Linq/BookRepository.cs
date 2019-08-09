225e2d6eec7cc04809d93955bea746735cf13ac5
Paddy Mc Glone <patrick.mcglone@civica.co.uk>
2019-08-09 09:12:37 +0100

    Adding seeding

---
+++ Linq/BookRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Linq
+{
+    public class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book {Title = "Lowdown transporters",          Price = 10.99},
+                new Book {Title = "Transporter HQ",                Price = 9.99},
+                new Book {Title = "VW Transporter Haynes mannual", Price = 6.00}
+            };
+        }
+    }
+}
