64c805800a0c8da611aa0711df839a21729f8c0d
Paddy Mc Glone <patrick.mcglone@civica.co.uk>
2019-08-09 09:34:11 +0100

    Update Program.cs
    - Adding a select projection.

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -11,10 +11,10 @@ namespace Linq
         static void Main(string[] args)
         {
             var books = new BookRepository().GetBooks();
-            var cheapBooks = books.Where(b => b.Price < 10);
+            var cheapBooks = books.Where(b => b.Price < 10).OrderBy(b => b.Title).Select(b => b.Title);
 
             foreach (var book in cheapBooks)
-                Console.WriteLine(book.Title + ' ' + book.Price);
+                Console.WriteLine(book);
         }
     }
 }
