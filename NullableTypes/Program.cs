9a995cf57ead40ce54e50f9ec9ca07389cb5b35c
Paddy McGlone <paddymcglone@gmail.com>
2019-08-10 19:34:11 +0100

    Update Program.cs
    - Adding null collessing types.

--- NullableTypes/Program.cs
+++ NullableTypes/Program.cs
@@ -12,9 +12,21 @@ namespace NullableTypes
             // Adding the Nullable<> prefix allows the null value to be held.
             Nullable<DateTime> dateTime = null;
 
-            Console.WriteLine(dateTime.GetValueOrDefault());
-            Console.WriteLine(dateTime.HasValue);
-            Console.WriteLine(dateTime.Value);
+            //Console.WriteLine(dateTime.GetValueOrDefault());
+            //Console.WriteLine(dateTime.HasValue);
+            //Console.WriteLine(dateTime.Value);
+
+            DateTime? newDate = null;
+            DateTime date2;
+
+            if (newDate != null)            
+                date2 = newDate.GetValueOrDefault();            
+            else            
+                date2 = DateTime.Today;
+
+            // There is a better way to write this code
+
+            date2 = newDate ?? DateTime.Today;
         }
     }
 }
