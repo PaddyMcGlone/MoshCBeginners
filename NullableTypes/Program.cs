66a0e0065916aca62e35c216288f47fe0e7120ae
Paddy McGlone <paddymcglone@gmail.com>
2019-08-10 18:52:12 +0100

    Adding new nullable project
    - Adding the new project

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
