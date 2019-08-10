fb8da0117412f95fde92e694dc1df0055cdb3230
Paddy McGlone <paddymcglone@gmail.com>
2019-08-10 20:15:06 +0100

    Update Program.cs

--- Dynamics/Program.cs
+++ Dynamics/Program.cs
@@ -7,15 +7,20 @@ namespace Dynamics
         public static void Main(string[] args)
         {
             // A simple example of dynamic types
-            dynamic myString = "PaddyMcGlone";
-            myString = 100;
+            //dynamic myString = "PaddyMcGlone";
+            //myString = 100;
 
 
-            dynamic a = 10;
-            dynamic b = 100;
+            //dynamic a = 10;
+            //dynamic b = 100;
 
             // At compile time, result will be set to type dynamic.
-            var result = b - a;        
+            //var result = b - a;
+
+            int i = 10;
+            dynamic d = i;
+
+            Console.WriteLine(d);
         }
     }
 }
