db52bfa5e5ec26ff9a695334940008290bb5a174
Paddy McGlone <paddymcglone@gmail.com>
2019-08-11 18:54:47 +0100

    Adding seeded data.

---
+++ ExceptionHandling/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace ExceptionHandling
+{
+    public class Calculator
+    {
+        public int Divide(int inumerator, int demoninator)
+        {
+            return inumerator / demoninator;
+        }
+    }
+}
