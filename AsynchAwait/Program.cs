01538c1255b643367d4ac263dac422e9c0384b3e
Paddy McGlone <paddymcglone@gmail.com>
2019-08-20 19:28:28 +0100

    Update Program.cs

--- AsynchAwait/Program.cs
+++ AsynchAwait/Program.cs
@@ -4,7 +4,7 @@ namespace AsynchAwait
 {
     class MainClass
     {
-        public async static void Main()
+        public static void Main()
         {
             var engine = new DownloadEngine();
             Console.WriteLine("Doing the first action, next i will download the page");
@@ -12,13 +12,13 @@ namespace AsynchAwait
             //engine.DownloadHtml(@"https://www.thetimes.co.uk/");
             //engine.DownloadHtmlAsync(@"https://www.thetimes.co.uk/");
 
-            var html = await engine.GetHtml("http://msdn.microsoft.com");
+            //var html = await engine.GetHtml("http://msdn.microsoft.com");
 
             Console.WriteLine(html.Substring(0,10));
 
             Console.WriteLine("The page has been downloaded!");
 
-            Console.WriteLine("All done");
+            Console.WriteLine("All done");            
         }
     }
 }
